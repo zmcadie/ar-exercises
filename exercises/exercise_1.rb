16cb2b763e690361bb814e56cf5284f788b83f51
Zola <zolamcadie@gmail.com>
2017-08-01 18:58:18 +0000

    added data to stores table

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create({name: 'Burnaby', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true})
+Store.create({name: 'Richmond', annual_revenue: 1_260_000, mens_apparel: false, womens_apparel: true})
+Store.create({name: 'Gastown', annual_revenue: 190_000, mens_apparel: true, womens_apparel: false})
+
+puts Store.count
