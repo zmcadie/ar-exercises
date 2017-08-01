6e1bbe7d15a30f4a685d9078deddd4489c05ccf5
Zola <zolamcadie@gmail.com>
2017-08-01 22:34:10 +0000

    added destroy limitation to store class

--- lib/store.rb
+++ lib/store.rb
@@ -3,11 +3,20 @@ class Store < ActiveRecord::Base
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
   validate :must_carry_clothes, on: :create
+  before_destroy :has_employees?
 
   def must_carry_clothes
     if (!mens_apparel.present? || mens_apparel == false) &&
        (!womens_apparel.present? || womens_apparel == false)
       errors.add(:apparel, "must sells mens or womens apparel (or both)")
+    end
+  end
+
+  private
+  def has_employees?
+    if self.employees.count > 0
+      errors.add(:destroy, "You monster, people work here! You can't destroy this store!")
+      throw :abort
     end
   end
 end
