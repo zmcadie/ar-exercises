ffe92c0a7ef0145b890a27fef0de0d285a9ebc32
Zola <zolamcadie@gmail.com>
2017-08-01 20:11:42 +0000

    added validations to store class

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+  validate :must_carry_clothes, on :create
+
+  def must_carry_clothes
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:name, "does not carry clothes")
 end
